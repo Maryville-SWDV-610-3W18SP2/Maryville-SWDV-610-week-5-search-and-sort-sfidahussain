9e7e563d340c850ed0c940c9aa3c2d9241b7178c
sfidahussain <6472383+sfidahussain@users.noreply.github.com>
2018-04-15 11:18:50 -0500

    added comments and fine tuning
    added comments and fine tuning

--- perfect_hashing.py
+++ perfect_hashing.py
@@ -3,17 +3,29 @@
 # the perfect hash algorithm.
 
 def hash(list, tablesize):
+    # Initializing hash table. I used a dictionary, setting each key through looping based on the tablesize, and setting each item intially to None
+    # This idea was derived from:
+    # https://docs.python.org/3/tutorial/datastructures.html
+    # https://edhenry.github.io/2016/12/21/Hashing-in-Python/
+    
     htable = dict([(i,None) for i,x in enumerate(range(tablesize))])
+    
+    # From the lecture: used ordinal hashing.
     sum = 0
     for name in list: 
         for pos in range(len(name)):
             sum = sum + ord(name[pos])
+        # Assigns a hash value based on the sum of the ordinal hashing moding from the tablesize to fit in the hash table
         slot =  sum % tablesize
+        # If there's nothing in that spot, place the name there. 
         if htable[slot] == None:
            htable[slot] = name
+        # If there is something already in that spot, a collision occurs. 
         else:
+            # Used linear probing. Go the next slot until a spot is open. 
             while htable[slot] != None:
                 slot = slot + 1 % tablesize
+                print('Collision occured with {0} now placing in slot {1}'.format(name, slot))
             htable[slot] = name
     return htable
 
--- sort.py
+++ sort.py
@@ -11,7 +11,13 @@ def bubbleSort(alist):
                 temp = alist[i]
                 alist[i] = alist[i+1]
                 alist[i+1] = temp
+                print('Exchanging {0} and {1}'.format(alist[i], alist[i+1]))
+            else:
+                print('No Exchange')
+        print('{} filled in final slot'.format(alist[passnum]))
         print(alist)
+    print('{} ok. List is sorted'.format(alist[0]))
+        
         
 def selectionSort(alist):
     for fillslot in range(len(alist)-1,0,-1):
@@ -22,9 +28,13 @@ def selectionSort(alist):
             temp = alist[fillslot]
             alist[fillslot] = alist[positionOfMax]
             alist[positionOfMax] = temp
+        print('{0} is largest'.format(alist[fillslot]))
         print(alist)
+    print('{0} ok. List is sorted'.format(alist[0]))
 
 def insertionSort(alist):
+    print(alist)
+    print('Assume {} is a sorted list of 1 item'.format(alist[0]))
     for index in range(1,len(alist)):
         currentvalue = alist[index]
         position = index
@@ -33,15 +43,18 @@ def insertionSort(alist):
             position = position-1
         alist[position]=currentvalue
         print(alist)
+        print('Inserted {}'.format(currentvalue))
 
 def main():
     alist = [10, 5, 3, 4, 1, 8, 9, 7, 2, 6]
-    print('Bubble Sort')
+    print('Bubble Sort: ')
     bubbleSort(alist)
     alist = [10, 5, 3, 4, 1, 8, 9, 7, 2, 6]
+    print('')
     print('Selection Sort')
     selectionSort(alist)
     alist = [10, 5, 3, 4, 1, 8, 9, 7, 2, 6]
+    print('')
     print('Insertion Sort')
     insertionSort(alist)
 
